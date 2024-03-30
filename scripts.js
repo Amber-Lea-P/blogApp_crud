88afad34a973fac70eb869d37e7a5159ceced13d
Amber Lea <112793091+Amber-Lea-P@users.noreply.github.com>
2024-03-30 05:35:03 +0200

    completion of script.js

--- scripts.js
+++ scripts.js
@@ -173,3 +173,194 @@ async function deletePost(postId, baseUrl) {
       alert('Delete post failed.');
     }
   }
+  // Update form
+function showUpdateForm(postId, title, content) {
+    const updateForm = `
+      <form id="update-form">
+          <input type="text" id="update-title" value="${title}">
+          <textarea id="update-content">${content}</textarea>
+          <button type="submit">Update post</button>
+      </form>
+      `;
+  
+    const postElement = document.getElementById(postId);
+    postElement.innerHTML += updateForm;
+  
+    const form = document.getElementById('update-form');
+    form.addEventListener('submit', (event) => updateForm(event, postId));
+  }
+//come back to the check is the updateForm the right variable?
+
+// Update post
+async function updatePost(event, postId) {
+    event.preventDefault();
+    const title = document.getElementById('update-title').value;
+    const content = document.getElementById('update-content').value;
+    const baseUrl = window.location.origin;
+  
+    // ensure that inputs are not empty
+    if (!title || !content) {
+      alert('Please fill in all fields 2.');
+      return;
+    }
+  
+    const updatedPost = {
+      title,
+      content,
+    };
+  
+    try {
+      const response = await fetch(`${baseUrl}/posts/${postId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${storedToken}`,
+        },
+        body: JSON.stringify(updatedPost),
+      });
+  
+      if (response.ok) {
+        alert('Update post successful!');
+        fetchPosts(baseUrl);
+      } else {
+        alert('Update post failed.');
+      }
+    } catch (error) {
+      console.error('An error occured during the fetch', error);
+      alert('Update post failed.');
+    }
+  }
+  // Register user
+async function registerUser(event, baseUrl) {
+    event.preventDefault();
+    const usernameInput = document.getElementById('register-username');
+    const passwordInput = document.getElementById('register-password');
+    const roleInput = document.getElementById('register-role');
+  
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const role = roleInput.value;
+  
+    // ensure that inputs are not empty
+    if (!username || !password || !role) {
+      alert('Please fill in all fields 3.');
+      return;
+    }
+  
+    const newUser = {
+      username,
+      password,
+      role,
+    };
+  
+    const res = await fetch(`${baseUrl}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    });
+  
+    const data = await res.json();
+  
+    if (data.success) {
+      alert('Registered successfully!');
+      // Clear input fields
+      usernameInput.value = '';
+      passwordInput.value = '';
+      roleInput.value = '';
+    } else {
+      alert('Registration failed.');
+    }
+  }
+  
+ // Login for user
+async function loginUser(event, baseUrl) {
+    event.preventDefault();
+    const usernameInput = document.getElementById('login-username');
+    const passwordInput = document.getElementById('login-password');
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+  
+    if (!username || !password) {
+      alert('Please fill in all fields 4.');
+      return;
+    }
+  
+    const user = {
+      username,
+      password,
+    };
+  
+    const res = await fetch(`${baseUrl}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+  
+    const data = await res.json();
+  
+    if (data.success) {
+      localStorage.setItem('jwtToken', data.token);
+      localStorage.setItem('userRole', data.role);
+      localStorage.setItem('username', username);
+  
+      // Close the hamburge menu if open
+      linksContainer.classList.toggle('active');
+      hamburger.classList.toggle('active');
+  
+      // Clear input fields
+      usernameInput.value = '';
+      passwordInput.value = '';
+  
+      location.reload();
+  
+      if (data.role === 'admin') {
+        showAdminFeatures();
+      }
+    } else {
+      alert('Login failed.');
+    }
+  } 
+
+  // Admin features
+function showAdminFeatures() {
+    const newPostDiv = document.getElementById('new-post-div');
+    if (newPostDiv) {
+      newPostDiv.style.display = 'flex';
+    }
+  
+    const allBtns = document.querySelectorAll('.btn');
+    allBtns.forEach((btn) => {
+      if (btn) {
+        btn.style.display = 'block';
+      }
+    });
+  }
+  
+  // Logout
+document.addEventListener('DOMContentLoaded', () => {
+    const baseUrl = window.location.origin;
+    const registerDiv = document.getElementById('register-div');
+    const loginDiv = document.getElementById('login-div');
+    const logoutDiv = document.getElementById('logout-div');
+    const logoutButton = document.getElementById('logout-button');
+  
+    if (storedToken) {
+      registerDiv.style.display = 'none';
+      loginDiv.style.display = 'none';
+      logoutDiv.style.display = 'flex';
+      logoutButton.addEventListener('click', () => {
+        localStorage.removeItem('jwtToken');
+        localStorage.removeItem('userRole');
+        localStorage.removeItem('username');
+        location.reload();
+      });
+    } else {
+      registerDiv.style.display = 'flex';
+      loginDiv.style.display = 'flex';
+      logoutDiv.style.display = 'none';
+    }
+  });
